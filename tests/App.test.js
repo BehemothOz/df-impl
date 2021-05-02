01d1f64b88fcf24e36b084893aa69e0f1d86f20b
behemothoz <behemothoz@yandex.ru>
2021-05-02 11:22:22 +0300

    fix lint

--- tests/App.test.js
+++ tests/App.test.js
@@ -2,9 +2,9 @@ import App from "../samples/App";
 
 test("jsx works", () => {
   expect(App).toEqual({
-    type: 'h1',
+    type: "h1",
     props: {
-      prop1: 'prop value',
+      prop1: "prop value",
       children: [
         {
           type: "TEXT_ELEMENT",
